e79a0e4c6edfb2079fa3e2a72de45a75585321f4
Piset Heang <piset2cambo@gmail.com>
2020-03-24 23:07:59 +0700

    test performance

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || 'KE Wallet' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -45,12 +45,8 @@ export default {
     ]
   },
   vuetify: {
-    // customVariables: ['~/assets/variables.scss'],
     defaultAssets: {icons: 'fa'},
     treeShake: true,
-    theme: {
-      light: true,
-    }
   },
   build: {
     extend (config, ctx) {
